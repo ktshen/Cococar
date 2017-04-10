49797a31824020bb77de9ed2bf488bfe4a752410
ktshen <bruce20036@gmail.com>
2017-04-11 02:18:41 +0800

    Add some basic web templates

--- cocoweb/map/urls.py
+++ cocoweb/map/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='map-home'),
-    url(r'^chat/$', views.chat, name='map-home'),
-    url(r'^marker/$', views.request_marker, name='map-marker'),
+    url(r'^marker$', views.request_marker, name='map-marker'),
+    url(r'^search$', views.search_record, name='search_record'),
+    url(r'^about$', views.about, name='about')
 ]
--- cocoweb/map/views.py
+++ cocoweb/map/views.py
@@ -5,6 +5,7 @@ import datetime
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 from .models import Marker
 
 
@@ -12,8 +13,13 @@ from .models import Marker
 def home(request):
     return render(request, "map.html")
 
-def chat(request):
-    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+
+def search_record(request):
+    pass
 
 
 @csrf_exempt
@@ -33,30 +39,29 @@ def request_marker(request):
             return HttpResponse(status=200)
         else:
             marker_id = rc.get("marker_id")
-            try:
-                marker = Marker.objects.get(marker_id=marker_id)
-            except Marker.DoesNotExist:
-                marker = Marker()
+            marker = Marker.objects.get_or_create(marker_id=marker_id)[0]
+            print(marker)
             for key in rc.keys():
                 setattr(marker, key, rc[key])
             marker.save()
             return HttpResponse(status=200)
     else:
         queryset = Marker.objects.filter(marker_id__istartswith="marker") \
-            .filter(create__gte=datetime.datetime.now() - datetime.timedelta(hours=1))
-        queryset += Marker.objects.filter(marker_id__istartswith="user") \
-            .exclude(live_ending_time__isnull=False)
+                                 .filter(create__gte=timezone.now() - datetime.timedelta(hours=1))
+        queryset2 = Marker.objects.filter(marker_id__istartswith="user")\
+                                     .exclude(live_ending_time__isnull=False)
         response = dict()
-        for m in queryset:
-            d = {
-                "marker_id": m.marker_id,
-                "user_id": m.user_id,
-                "longitude": m.longitude,
-                "latitude": m.latitude,
-                "url": m.url,
-                "talk": m.talk,
-            }
-            response.update(m)
+        for q in [queryset, queryset2]:
+            for m in q:
+                d = {
+                    "marker_id": m.marker_id,
+                    "user_id": m.user_id,
+                    "longitude": m.longitude,
+                    "latitude": m.latitude,
+                    "url": m.url,
+                    "talk": m.talk,
+                }
+                response.update(d)
         return JsonResponse(data=response)
 
 
