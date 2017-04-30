3b32d407cc8ce2f5da9b10e3d578709a55de2f00
Timmy <he02515939@gmail.com>
2017-04-30 12:47:37 +0800

    fix bug

--- cococar/app/src/main/java/com/tom/cococar/MapsActivity.java
+++ cococar/app/src/main/java/com/tom/cococar/MapsActivity.java
@@ -187,7 +187,9 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
-     */    public void onMapReady(GoogleMap googleMap) {
+     *
+     */
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         //位置權限
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -732,10 +734,10 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
     protected void onResume(){
         super.onResume();
         Log.d("bag", "url1");
-        rand = UUID.randomUUID().toString();
         liverand = "user-"+rand;
         StopTask stop = new StopTask();
         stop.executeOnExecutor(THREAD_POOL_EXECUTOR,liverand);
+        rand = UUID.randomUUID().toString();
     }
 
 }
